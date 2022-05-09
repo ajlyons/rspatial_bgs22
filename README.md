dd7d3f6b44b55fca38012f0fec66e2efb617509f
Andy Lyons <lyons.andy@gmail.com>
2022-05-09 07:06:31 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Spatial Analysis in R 
+
+These are slides for the Spatial Analysis in R workshop at the Society for Conservation GIS 2021 Annual Conference. 
+
+
+
---
+++ docs/scripts/install_required_pkgs.R
@@ -0,0 +1,33 @@
+## Install sf before everything else
+## (b/c on Linux it installs by itself fine, but not as a dependency)
+if (!require(sf)) install.packages("sf")
+
+## Required packages
+pkgs_req <- c("sp", "tmap", "tmaptools", "leaflet", "ggmap", "maptools", 
+              "RColorBrewer", "raster", "maps", "tidyverse",  "jsonlite", 
+              "rasterVis", "GISTools", "nngeo", "lwgeom", "remotes", 
+              "conflicted", "tigris", "tidycensus", "stars",
+              "googlesheets4", "httr", "rjson", "RSocrata", "usethis", "rinat")
+
+## See which ones are missing
+(pkgs_missing <- pkgs_req[!(pkgs_req %in% installed.packages()[,"Package"])])
+
+## Install missing ones
+if (length(pkgs_missing)) install.packages(pkgs_missing, dependencies=TRUE)
+
+## Re-run the check for missing packages
+pkgs_missing <- pkgs_req[!(pkgs_req %in% installed.packages()[,"Package"])]
+if (length(pkgs_missing)==0) cat("ALL PACKAGES WERE INSTALLED SUCCESSFULLY \n")
+
+##########################################
+## TIPS
+##
+## If you are prompted by the question, 'Do you want to install from sources the 
+## package which needs compilation?', select 'No'.
+##
+## If you get an error message that a package can't be installed because 
+## it's already loaded and can't be stopped, restart R (Session >> Restart R), 
+## and try again.
+##
+##########################################
+
